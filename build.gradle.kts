b8707d1dcc71edd0fbbe0bb46b6c874f51781a06
Cheon Jaeung <cheonjaewoong@gmail.com>
2025-06-21 14:06:28 +0900

    Apply paparazzi plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.paparazzi) apply false
     alias(libs.plugins.maven.publish) apply false
 }
 
--- grid/build.gradle.kts
+++ grid/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.android.library)
+    alias(libs.plugins.paparazzi)
     alias(libs.plugins.maven.publish)
 }
 
