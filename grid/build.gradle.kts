c2f1978516f1943a8770296e5d837fe06ae6d205
Cheon Jaeung <cheonjaewoong@gmail.com>
2025-07-06 13:34:34 +0900

    Change publishToMavenCentral(SonatypeHost) to publishToMavenCentral()

--- grid/build.gradle.kts
+++ grid/build.gradle.kts
@@ -1,4 +1,3 @@
-import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
@@ -70,7 +69,7 @@ android {
 }
 
 mavenPublishing {
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    publishToMavenCentral()
     signAllPublications()
     coordinates("${project.group}", "grid", "${project.version}")
 
