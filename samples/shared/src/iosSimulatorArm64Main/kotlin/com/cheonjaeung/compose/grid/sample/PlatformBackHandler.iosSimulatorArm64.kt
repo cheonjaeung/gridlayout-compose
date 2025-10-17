2ca31fe73cd7daeb806019289bd6b0ac5063a64c
Cheon Jaeung <cheonjaewoong@gmail.com>
2025-10-17 20:21:56 +0900

    Merge iOS source sets to iosMain

--- samples/shared/src/iosSimulatorArm64Main/kotlin/com/cheonjaeung/compose/grid/sample/PlatformBackHandler.iosSimulatorArm64.kt
+++
@@ -1,6 +0,0 @@
-package com.cheonjaeung.compose.grid.sample
-
-import androidx.compose.runtime.Composable
-
-@Composable
-internal actual fun PlatformBackHandler(enabled: Boolean, onBack: () -> Unit) {}
--- samples/shared/src/iosX64Main/kotlin/com/cheonjaeung/compose/grid/sample/MainViewController.kt
+++
@@ -1,6 +0,0 @@
-package com.cheonjaeung.compose.grid.sample
-
-import androidx.compose.ui.window.ComposeUIViewController
-
-@Suppress("FunctionName", "unused")
-fun MainViewController() = ComposeUIViewController { SampleList() }
