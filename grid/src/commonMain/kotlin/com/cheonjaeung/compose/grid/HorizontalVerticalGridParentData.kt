2ab9ade21bf6f9960f25969483341bb0d74bf0dd
Cheon Jaeung <cheonjaewoong@gmail.com>
2025-11-23 15:03:54 +0900

    Remove useless DefaultAlignment constant

--- grid/src/commonMain/kotlin/com/cheonjaeung/compose/grid/HorizontalVerticalGridParentData.kt
+++ grid/src/commonMain/kotlin/com/cheonjaeung/compose/grid/HorizontalVerticalGridParentData.kt
@@ -7,9 +7,5 @@ import androidx.compose.ui.Alignment
  */
 internal data class HorizontalVerticalGridParentData(
     var span: (GridItemSpanScope.() -> Int)? = null,
-    var alignment: Alignment = DefaultAlignment
-) {
-    companion object {
-        internal val DefaultAlignment: Alignment = Alignment.TopStart
-    }
-}
+    var alignment: Alignment? = null
+)
